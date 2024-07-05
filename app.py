09f0f05755bc4c61b78a6f81f0b3ec28224dacef
leeyounghuncom <web3dao@daum.net>
2024-07-05 11:10:16 +0900

    html 파일쪽은 테스트용 이묘 예지님 HTML오면 병합 하며 HTML 맞게 app.py 소스도 수정 하겠습니다.

--- app.py
+++ app.py
@@ -3,8 +3,6 @@
 pip install flask
 pip install flask_sqlalchemy
 '''
-
-
 from flask import Flask, render_template, request
 import random
 from flask_sqlalchemy import SQLAlchemy
@@ -40,14 +38,40 @@ with app.app_context():
 #################################################################
 ##################################################################
 
-@app.route("/")
+# @app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
 
-
     ####### 코드 작성 ############
+    rsp = ["가위", "바위", "보"]
+    rsp_data = {} #데이터
 
+    if request.method == "POST":
+        #POST 방식으로 저장 할 경우 form.get
+        # guest = request.form.get("rsp_guest") #게스트 input name="rsp_guest"
+        guest = "가위"  # 임시
+        bot = random.choice(rsp) #봇
 
-    return render_template("game.html")
+        # 비겼습니다 졌습니다 이겼습니다 이 단어는 임의적 데이터
+        if guest == bot:
+            r = "비겼습니다"
+        elif ((guest == "가위" and bot == "바위") or (guest == "바위" and bot == "보") or (guest == "보" and bot == "가위")):
+            r = "졌습니다"
+        else:
+            r = "이겼습니다"
+
+        gh = RockPaperSissor(user=guest, com=bot, result=r)
+        db.session.add(gh)
+        db.session.commit()
+
+        # 임시
+        q = RockPaperSissor.query.order_by(RockPaperSissor.id.desc()).all(); #오른찻순 수정
+
+        # 임의적 메시지
+        rsp_data["msg"] = f"사용자 : {guest} 컴퓨터 : {bot} {r}"
+        rsp_data["gh"] = q
+
+    return render_template("game.html", data=rsp_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
